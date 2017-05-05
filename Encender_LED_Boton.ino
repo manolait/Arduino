5422a6182ffc631013d87ac5cef81643a0010422
Manuel <manolomurodriguez@gmail.com>
2017-05-05 17:31:19 +0200

    Encender_LED_Boton

---
+++ Encender_LED_Boton.ino
@@ -0,0 +1,19 @@
+int PULSADOR = 2;
+int LED = 3;
+int estado = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PULSADOR, INPUT);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(digitalRead(PULSADOR)==LOW);
+  estado = digitalRead(LED);
+  digitalWrite(LED, !estado);
+  while(digitalRead(PULSADOR)==HIGH);
+  
+}
